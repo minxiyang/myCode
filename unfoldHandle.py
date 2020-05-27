e9d04815fcd34ccbec5e7d1403fc3158e0219b6d
[Minxi <[yhzz598@gmail.com]>
2020-05-27 15:57:36 -0400

    update

--- unfoldHandle.py
+++ unfoldHandle.py
@@ -46,23 +46,29 @@ class unfoldHandle(object):
 		SVD=ROOT.TDecompSVD(m)
 		return SVD.Condition()
 	def doUnfold(self,data,back=0,regdiff=0,iteration=0):
-		self.data=data
+		self.data=data.Clone()
 		if back!=0:
-			data.Add(back,-1.)
+			self.data.Sumw2()
+			back.Sumw2()
+			self.data.Add(back,-1.)
 			self.back=back.Clone()
+			for i in range(self.data.GetNbinsX()+2):
+				if self.data.GetBinContent(i)<0:
+					self.data.SetBinContent(i,0)
 		if regdiff!=0:
-			Unfold=RooUnfoldBayes(self.response,data,1,False,self.name+"_reg",self.name+"_reg")
-			h2=data.Clone()
-        		for i in range(100):
+			Unfold=RooUnfoldBayes(self.response,self.data,1,False,self.name+"_reg",self.name+"_reg")
+			h2=self.data.Clone()
+        		for i in range(9999):
                 		h1=Unfold.Hreco().Clone()
                 		vaild=0
                 		val=0
                 		for j in range(1,h2.GetNbinsX()+1):
-                        		if h2.GetBinContent(j)!=0:
+                        		if h2.GetBinContent(j)>0:
                                 		val=h1.GetBinContent(j)/h2.GetBinContent(j)
                                 		val=abs(val-1)
-                        		if vaild<val:
-                                		vaild=val
+                        			if vaild<val:
+                                			vaild=val
+				print vaild
                 		if vaild<regdiff:
                         		print "%d times iteration" %i
 					self.UnfoldedData=h1
@@ -70,14 +76,16 @@ class unfoldHandle(object):
 				else:
 					h2=h1.Clone()
                         		Unfold.Reset()
-                        		Unfold=RooUnfoldBayes(self.response,data,i+2,False,self.name,self.name)
+                        		Unfold=RooUnfoldBayes(self.response,self.data,i+2,False,self.name+"_reg",self.name+"_reg")
 		elif iteration !=0:
 			s="iter_%d"%iteration
-			Unfold=RooUnfoldBayes(self.response,data,iteration,False,self.name+s,self.name+s)
+			Unfold=RooUnfoldBayes(self.response,self.data,iteration,False,self.name+s,self.name+s)
 			h1=Unfold.Hreco().Clone()
 			return h1
 		else:
-			Unfold=RooUnfoldInvert(self.response,data,self.name+"_invert",self.name+"_invert")
+			Unfold=RooUnfoldInvert(self.response,self.data,self.name+"_invert",self.name+"_invert")
+			Unfold.SetMeasured(self.data)
+			self.Unfold=Unfold
 			h1=Unfold.Hreco().Clone()
 			self.UnfoldedData=h1
 			return h1
